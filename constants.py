3f335ed34531d2b05820f0d88fafa5ff79886f32
Wilfried <github1@grousson.eu>
2023-01-12 19:43:37 +0100

    add constant

--- constants.py
+++ constants.py
@@ -4,12 +4,14 @@
  # @ License: MIT
  # @ Description: """This module defines project-level constants."""
  '''
+import os
 import logging
 
 APP_NAME = "CGMeter"
-APP_VERSION = "0.1.0"
+APP_VERSION = "0.2.0"
 APP_AUTHOR = "Wilfried Grousson"
 APP_CONFIG_DIR = "config"
-APP_CG_CONFIG = "cgconfig.json"
+APP_CG_CONFIG_NAME = "cgconfig.json"
 LOG_LEVEL = logging.DEBUG
-EMULATE_HX711 = False
+EMULATE_HX711 = True
+APP_CG_FILENAME = os.path.join(os.getcwd(),APP_CONFIG_DIR,APP_CG_CONFIG_NAME)
